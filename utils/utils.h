ce822e7d84366cb0073ea5f2808989d6bac935c7
Einestein <106955055+wlzhao22@users.noreply.github.com>
2026-04-27 15:10:49 +0800

    Add files via upload

---
+++ utils/utils.h
@@ -0,0 +1,60 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <chrono>
+#include <fcntl.h>
+
+
+#include <cuda_runtime.h>
+
+#include <iostream>
+
+#define gpuErrchk(ans) {gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort=true) {
+  if(code != cudaSuccess)
+  {
+   fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+   if (abort) exit(code);
+  }
+ }
+
+off_t caclulate_filesize(const char* chFileName)
+{
+	int fd = -1;
+
+	if ((fd = open(chFileName, O_RDONLY , (mode_t)0 )) == -1)
+	{
+		perror("Error opening file for writing");
+		exit(EXIT_FAILURE);
+	}
+
+	long cur_pos = 0L;
+	off_t file_size = 0L;
+
+	// file would be opened with file offset set to the very beginning
+	cur_pos = lseek(fd, 0, SEEK_CUR);
+	file_size = lseek(fd, 0, SEEK_END);
+	lseek(fd, cur_pos, SEEK_CUR);
+	close(fd);
+	return file_size;
+}
+
+unsigned long long log_message (const char* message)
+{
+	const std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+	const std::time_t ctimenow_obj = std::chrono::system_clock::to_time_t(now );
+	auto duration = now.time_since_epoch();
+	auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+	struct tm *local = localtime(&ctimenow_obj);
+	std::cout <<  local->tm_mday << ":" << local->tm_mon  << ":" << local->tm_year << ":" \
+			  <<  local->tm_hour << ":" << local->tm_min << ":" << local->tm_sec << " [" << millis << "] : " << message << std::endl;
+	return millis;
+}
+
+
+#endif
+
+
